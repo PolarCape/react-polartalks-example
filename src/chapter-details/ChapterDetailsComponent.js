cecc7e184eb8db5908dfd751d89ad048daf01b5c
Petar Blazevski <petar.blazevski@gmail.com>
2017-07-02 20:32:04 +0200

    Lifecycle diagram

--- src/chapter-details/ChapterDetailsComponent.js
+++ src/chapter-details/ChapterDetailsComponent.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import picture from './react-lifecycle.png';
 
 class ChapterDetailsComponent extends Component {
+
+    
+    componentWillMount() {
+        console.log('COMPONENT WILL MOUNT');
+    }
+
+    componentDidMount() {
+        console.log('COMPONENT DID MOUNT');
+    }
+    
+    
     render() {
+        console.log('RENDER');
         return (
             <section>
-                <div className="container">
-                    
+                <div className="container has-text-centered">
+                    <h1 className="title">Lifecycle and Rendering Component</h1>
+                    <img src={picture} alt="Lifecycle Diagram"/>
                 </div>
             </section>
         );
