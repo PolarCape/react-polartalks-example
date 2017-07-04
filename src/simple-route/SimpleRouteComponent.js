05cdccaac613aa9f4449f944db3ffd92f14f7211
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2017-07-04 19:08:55 +0200

    added form inputs

--- src/simple-route/SimpleRouteComponent.js
+++ src/simple-route/SimpleRouteComponent.js
@@ -11,6 +11,17 @@ class SimpleRouteComponent extends Component {
                             <input name="name" className="input" type="text" placeholder="John" />
                         </p>
                     </div>
+                    <div className="field">
+                        <label className="label">Surname</label>
+                        <p className="control">
+                            <input name="name" className="input" type="text" placeholder="Doe" />
+                        </p>
+                    </div>
+                    <div className="field is-grouped">
+                        <p className="control">
+                            <button className="button is-primary">Submit</button>
+                        </p>
+                    </div>
                 </div>
             </section>
         );
