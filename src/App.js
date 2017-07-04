c91c44fdf0668a8e8a968941132859cbc883a340
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2017-07-04 19:07:42 +0200

    added form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-import Guard from './Guard';
-
 import NavigationComponent from './navigation/NavigationComponent';
 import LandingComponent from './landing/LandingComponent';
 import SimpleRouteComponent from './simple-route/SimpleRouteComponent';
-import RouteWithParamComponent from './route-with-param/RouteWithParamComponent';
-import GuardedRouteComponent from './guarded-route/GuardedRouteComponent';
 
 class App extends Component {
   render() {
@@ -16,11 +12,8 @@ class App extends Component {
       <Router>
         <div className="App">
           <NavigationComponent />
-
-          <Route exact path="/" component={LandingComponent} />
-          <Route path="/simple-route" component={SimpleRouteComponent} />
-          <Route path="/route-with-param/:someText" component={RouteWithParamComponent} />
-          <Route path="/guarded-route" component={Guard()(GuardedRouteComponent)} />
+          <LandingComponent />
+          <SimpleRouteComponent />
         </div>
       </Router>
     );
--- src/simple-route/SimpleRouteComponent.js
+++ src/simple-route/SimpleRouteComponent.js
@@ -4,8 +4,13 @@ class SimpleRouteComponent extends Component {
     render() {
         return (
             <section>
-                <div className="container has-text-centered">
-                    <h1 className="title">This is a simple route</h1>
+                <div className="form-wrapper container has-text-centered">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <p className="control">
+                            <input name="name" className="input" type="text" placeholder="John" />
+                        </p>
+                    </div>
                 </div>
             </section>
         );
