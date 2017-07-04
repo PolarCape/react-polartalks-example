3551367f37dcfdbc34dac86778e1ace54745a1a0
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2017-07-04 19:20:05 +0200

    added handlers for inputs

--- src/simple-route/SimpleRouteComponent.js
+++ src/simple-route/SimpleRouteComponent.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 
 class SimpleRouteComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {name: '', surname: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('Form submitted: ', this.state);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <section>
@@ -8,18 +29,28 @@ class SimpleRouteComponent extends Component {
                     <div className="field">
                         <label className="label">Name</label>
                         <p className="control">
-                            <input name="name" className="input" type="text" placeholder="John" />
+                            <input name="name"
+                                   className="input"
+                                   type="text"
+                                   placeholder="John"
+                                   value={this.state.name}
+                                   onChange={this.handleChange}/>
                         </p>
                     </div>
                     <div className="field">
                         <label className="label">Surname</label>
                         <p className="control">
-                            <input name="name" className="input" type="text" placeholder="Doe" />
+                            <input name="surname"
+                                   className="input"
+                                   type="text"
+                                   placeholder="Doe"
+                                   value={this.state.surname}
+                                   onChange={this.handleChange}/>
                         </p>
                     </div>
                     <div className="field is-grouped">
                         <p className="control">
-                            <button className="button is-primary">Submit</button>
+                            <button onClick={this.handleSubmit} className="button is-primary">Submit</button>
                         </p>
                     </div>
                 </div>
